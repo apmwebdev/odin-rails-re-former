239725f76d2595b698ee3324d1611768174a1719
Austin Miller <austinmillerwebdev@gmail.com>
2022-12-28 06:04:00 -0500

    Add edit functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update]
   def new
     @user = User.new
   end
@@ -13,7 +14,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to edit_user_url(@user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
